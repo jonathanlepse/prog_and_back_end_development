1782db862302b9c6c644def7b5860c5100e702b2
jonathanlepse <jonathan.lepselter@gmail.com>
2015-12-11 01:14:24 +0000

    adding code review changes

--- Programming_Foundations/Lesson_2/mortgage_calculator.rb
+++ Programming_Foundations/Lesson_2/mortgage_calculator.rb
@@ -6,25 +6,26 @@ def display(msg)
 end
 
 def validate(num)
-  until num =~ /\A\d+\Z/
+  until num =~ /\A\d+\Z/ 
     display("Please try again. This time do not add any special characters to your numbers. (i.e. $, %)")
-    num = gets.chomp
+    num = gets.chomp 
   end
+  num # by extracting this to a method you must return the num so when you call the method below it returns a value, your vairables below are then set to this valid value 
 end
 
 display("Welcome to your mortgage signing.")
 
-display("How big of a mortgage do your need?")
+display("How big of a mortgage do you need?")
 original_loan_amount = gets.chomp
-validate(original_loan_amount)
+original_loan_amount = validate(original_loan_amount) # by setting this variable to the valid value returned by the method you can work with a validated number in your calculations below
 
 display("What duration would you like to apply for? (in years)")
 loan_duration = gets.chomp
-validate(loan_duration)
+loan_duration = validate(loan_duration) # same here setting variable to valid value
 
 display("What interest rate would you like to be charged?")
 interest_rate = gets.chomp
-validate(interest_rate)
+interest_rate = validate(interest_rate) # same here setting variable to valid value
 
 loan_duration_in_months = loan_duration.to_i * MONTHS_IN_YEAR
 apr = interest_rate.to_f / PERCENTAGE
