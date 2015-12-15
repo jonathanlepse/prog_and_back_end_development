4feaee0c8ab81fc5a162c65874ce9f07aa751a1b
jonathanlepse <jonathan.lepselter@gmail.com>
2015-12-15 18:59:43 +0000

    refactored with improved formatting

--- Programming_Foundations/Lesson_2/mortgage_calculator.rb
+++ Programming_Foundations/Lesson_2/mortgage_calculator.rb
@@ -6,11 +6,15 @@ def display(msg)
 end
 
 def validate(num)
-  until num =~ /\A\d+\Z/ 
+  until num =~ /\A\d+\Z/
     display("Please try again. This time do not add any special characters to your numbers. (i.e. $, %)")
-    num = gets.chomp 
+    num = gets.chomp
   end
-  num # by extracting this to a method you must return the num so when you call the method below it returns a value, your vairables below are then set to this valid value 
+  num # by extracting this to a method you must return the num so when you call the method below it returns a value, your vairables below are then set to this valid value
+end
+
+def format
+  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 end
 
 display("Welcome to your mortgage signing.")
@@ -23,14 +27,17 @@ display("What duration would you like to apply for? (in years)")
 loan_duration = gets.chomp
 loan_duration = validate(loan_duration) # same here setting variable to valid value
 
-display("What interest rate would you like to be charged?")
-interest_rate = gets.chomp
-interest_rate = validate(interest_rate) # same here setting variable to valid value
+mortgage_rates = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
+interest_rate = mortgage_rates.sample
+format
+display("You will charged an interest rate of: #{interest_rate}% for this loan.")
 
 loan_duration_in_months = loan_duration.to_i * MONTHS_IN_YEAR
-apr = interest_rate.to_f / PERCENTAGE
+apr = interest_rate / PERCENTAGE
 monthly_interest = original_loan_amount.to_i * apr / MONTHS_IN_YEAR
-monthly_payment = (original_loan_amount.to_i / loan_duration_in_months) + (original_loan_amount.to_i * monthly_interest)
+monthly_payment = (original_loan_amount.to_i / loan_duration_in_months) + monthly_interest
+time = Time.now
 
-display("This loan should be paid in full in #{loan_duration_in_months} months at a monthy interest rate of: #{monthly_interest}%.")
-display("Your monthly payment will be: $#{monthly_payment}")
+display("This loan should be paid in full on: #{time + (60 * 60 * 24 * 365 * loan_duration.to_i)}.")
+display("Your monthly payment will be: $#{sprintf('%0.2f', monthly_payment)}")
+format
