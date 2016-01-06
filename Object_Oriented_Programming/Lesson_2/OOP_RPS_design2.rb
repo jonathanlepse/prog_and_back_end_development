4bed025031434d409310cff3d00dc8032647a334
jonathanlepse <jonathan.lepselter@gmail.com>
2016-01-06 17:00:54 +0000

    refactors from OOP_RPS_design1, and rubocop

--- Object_Oriented_Programming/Lesson_2/OOP_RPS_design2.rb
+++ Object_Oriented_Programming/Lesson_2/OOP_RPS_design2.rb
@@ -3,19 +3,19 @@ class Move
   def initialize(value)
     @value = value
   end
-  
+
   def scissors?
     @value == 'scissors'
   end
-  
+
   def rock?
     @value == 'rock'
   end
-  
+
   def paper?
     @value == 'paper'
   end
-  
+
   def >(other_move)
     if rock?
       return true if other_move.scissors?
@@ -41,7 +41,7 @@ class Move
       return false
     end
   end
-  
+
   def to_s # this to_s method must be written in this class b/c we set the move instance variable from the player class equal to objects of this Move class and now we need to override to_s so it will return a value for us on string interpolation and not an object id
     @value
   end
@@ -49,11 +49,11 @@ end
 
 class Player
   attr_accessor :move, :name
-  
+ 
   def initialize
     set_name
   end
-  
+ 
 end
 
 class Human < Player
@@ -65,7 +65,7 @@ class Human < Player
         break unless person.empty?
         puts "You must enter a name."
       end
-      self.name = person # this will still string interpolate b/c it is set to a value of the person variable
+    self.name = person # this will still string interpolate b/c it is set to a value of the person variable
   end
   
   def choose
@@ -77,7 +77,7 @@ class Human < Player
         break if Move::VALUES.include?(choice)
         puts "You must enter either rock paper or scissors"
       end
-      self.move = Move.new(choice) # this will no longer string interpolate a value b/c it is now set to an object insetad, you need to override a to_s method in the Move class to get a value to be displayed on string interpolation
+    self.move = Move.new(choice) # this will no longer string interpolate a value b/c it is now set to an object insetad, you need to override a to_s method in the Move class to get a value to be displayed on string interpolation
   end
 end
 
@@ -85,7 +85,7 @@ class Computer < Player
   def set_name
     self.name = ["R2D2", "Optimus Prime", "Megatron", "Johnny5", "Terminator"].sample
   end
-  
+
   def choose
     self.move = Move.new(Move::VALUES.sample) # this will no longer string interpolate a value b/c it is now set to an object insetad, you need to override a to_s method in the MOve class to get a value to be displayed on string interpolation
   end
@@ -93,21 +93,21 @@ end
 
 class RPSGame
   attr_accessor :human, :computer
-  
+
   def initialize
     @human = Human.new 
     @computer = Computer.new
   end
-  
+
   def display_welcome_message
     puts "Welcome to Rock,Paper,Scissors #{human.name}!"
   end
-  
+
   def display_winner
     puts "------------------------------------------"
     puts "#{human.name} chose #{human.move}" # here we are now overriding the built in to_s and using our own to_s written in the move class b/c human.move returns an object and not a value b/c we set it equal to a new object of the Move class
     puts "#{computer.name} chose #{computer.move}"# here we are now overriding the built in to_s and using our own to_s written in the move class b/c human.move returns an object and not a value b/c we set it equal to a new object of the Move class
-    
+
     if human.move > computer.move
       puts "#{human.name} won!"
     elsif human.move < computer.move
@@ -115,14 +115,14 @@ class RPSGame
     else
       puts "It's a tie!"
     end
-    
+
     puts "------------------------------------------"
   end
-  
+
   def display_goodbye_message
     puts "Thanks for Playing #{human.name}. Good-Bye."
   end
-  
+
   def play_again?
     answer = nil
     loop do
@@ -135,7 +135,7 @@ class RPSGame
     return true if answer == 'y'
     return false
   end
-  
+
   def play
     display_welcome_message
     loop do
@@ -147,7 +147,7 @@ class RPSGame
     puts "------------------------------------------"
     display_goodbye_message
   end
-  
+
 end
 
 RPSGame.new.play
